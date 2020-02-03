965e4df8ec0b31b538ea40dcf77e6a9803ab1fc5
Matthew Doles <matteddied@gmail.com>
2020-02-03 11:29:25 -0500

    4.5 - State

--- react-refresher-starting-project/src/App.js
+++ react-refresher-starting-project/src/App.js
@@ -1,18 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./App.css";
 import GoalList from "./components/GoalList/GoalList";
 import NewGoal from "./components/NewGoal/NewGoal";
 
 const App = () => {
-  const courseGoals = [
+  const [courseGoals, setCourseGoals] = useState([
     { id: "cg1", text: "Finish the course" },
     { id: "cg2", text: "Learn all about the Course Main Topic" },
     { id: "cg3", text: "Help other students in the Course Q&A" }
-  ];
+  ]);
 
   const addNewGoalHandler = newGoal => {
-    courseGoals.push(newGoal);
+    //setCourseGoals(courseGoals.concat(newGoal));
+    setCourseGoals(prevCourseGoals => prevCourseGoals.concat(newGoal));
   };
 
   return (
