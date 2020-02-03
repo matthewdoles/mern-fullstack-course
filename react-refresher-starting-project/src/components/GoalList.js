7e79078a10b4b6f202378c0215fc2fddcda9a84f
Matthew Doles <matteddied@gmail.com>
2020-02-03 10:56:39 -0500

    4.3 - Components & Props

---
+++ react-refresher-starting-project/src/components/GoalList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './GoalList.css'
+
+const GoalList = props => {
+  console.log(props.goals);
+  return (
+    <ul className="goal-list">
+
+    </ul>
+  );
+};
+
+export default GoalList;
