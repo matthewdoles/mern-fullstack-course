456abbd6c0c111471d7b6de8fe0c95e439b21edb
Matthew Doles <matteddied@gmail.com>
2020-02-05 14:00:38 -0500

    5.3 - Adding a UsersList Page

--- react-frontend/src/user/pages/Users.js
+++ react-frontend/src/user/pages/Users.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
+import UsersList from '../components/UsersList';
+
 const Users = () => {
-  return <h2>Users Works!</h2>;
+  const USERS = [
+    {
+      id: 'u1',
+      name: 'Matthew Doles',
+      image: 'https://avatars1.githubusercontent.com/u/38084552?s=460&v=4',
+      places: 3
+    }
+  ];
+
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
