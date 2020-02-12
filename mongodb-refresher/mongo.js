0a2a76c828a34456c144fb0929cff32ee3527aee
Matthew Doles <matteddied@gmail.com>
2020-02-12 17:41:26 -0500

    8.2 - Creating a Document with MongoDB

--- mongodb-refresher/mongo.js
+++ mongodb-refresher/mongo.js
@@ -1,13 +1,29 @@
-const mongoClient = require('mongodb').MongoClient;
+const MongoClient = require('mongodb').MongoClient;
 
-console.log(process.env.URL);
 const url = process.env.URL;
 
-const createProduct = async (req, res, next) => {};
+const createProduct = async (req, res, next) => {
+  const newProduct = {
+    name: req.body.name,
+    price: req.body.price
+  };
+
+  const client = new MongoClient(url);
+  try {
+    await client.connect();
+    const db = client.db();
+    const result = db.collection('products').insertOne(newProduct);
+  } catch (error) {
+    return res.json({ message: 'Could not store data.' });
+  };
+
+  client.close();
+  res.json(newProduct);
+};
 
 const getProducts = async (req, res, next) => {};
 
-module.export = {
+module.exports = {
   createProduct,
   getProducts
 };
