b01be8354d8dbf4d3d5ad193006c94918c4c0bf4
Matthew Doles <matteddied@gmail.com>
2020-02-12 18:08:45 -0500

    8.5 - Using Mongoose to Save Products

---
+++ mongodb-refresher/mongoose.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const Product = require('./models/product');
+
+mongoose
+  .connect(process.env.URL)
+  .then(() => {
+    console.log('Connected to database.');
+  })
+  .catch(() => {
+    console.log('Connection to database failed.');
+  });
+
+const createProduct = async (req, res, next) => {
+  const createdProduct = new Product({
+    name: req.body.name,
+    price: req.body.price
+  });
+
+  const result = await createdProduct.save();
+  res.json(result);
+};
+
+exports.createProduct = createProduct;
