8adb4151aa6ed38e25432eee450ef6c50f794a05
Matthew Doles <matteddied@gmail.com>
2020-02-13 13:37:37 -0500

    9.9 - Updating User Login

--- nodejs-backend/controllers/users-controller.js
+++ nodejs-backend/controllers/users-controller.js
@@ -20,9 +20,7 @@ const getUsers = (req, res, next) => {
 const signup = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return next(
-      new HttpError('Invalid input, please check your data', 422)
-    );
+    return next(new HttpError('Invalid input, please check your data', 422));
   }
 
   const { name, email, password, places } = req.body;
@@ -31,13 +29,15 @@ const signup = async (req, res, next) => {
   try {
     existingUser = await User.findOne({ email });
   } catch (error) {
-    return next(
-      new HttpError('Signup failed, please try again.', 500)
-    );
+    return next(new HttpError('Signup failed, please try again.', 500));
   }
+
   if (existingUser) {
     return next(
-      new HttpError('User already associated with that email, please login.', 422)
+      new HttpError(
+        'User already associated with that email, please login.',
+        422
+      )
     );
   }
 
@@ -47,7 +47,7 @@ const signup = async (req, res, next) => {
     image: 'https://avatars1.githubusercontent.com/u/38084552?s=460&v=4',
     password,
     places
-  })
+  });
 
   try {
     await newUser.save();
@@ -60,7 +60,7 @@ const signup = async (req, res, next) => {
   res.status(201).json({ user: newUser.toObject({ getters: true }) });
 };
 
-const login = (req, res, next) => {
+const login = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     throw new HttpError('Invalid input, please check your data', 422);
@@ -68,9 +68,15 @@ const login = (req, res, next) => {
 
   const { email, password } = req.body;
 
-  const identifiedUser = DUMMY_USERS.find(u => u.email === email);
-  if (!identifiedUser || identifiedUser.password !== password) {
-    throw new HttpError('Could not idnetify user', 401);
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email });
+  } catch (error) {
+    return next(new HttpError('Login failed, please try again.', 500));
+  }
+
+  if (!existingUser || existingUser.password !== password) {
+    return next(new HttpError('Invalid credentials, please try again.', 401));
   }
 
   res.json({ message: 'Logged in!' });
