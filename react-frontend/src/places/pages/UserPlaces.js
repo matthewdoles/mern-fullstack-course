aa828b29d7b9f5754eb78e4fe217b474720d77b8
Matthew Doles <matteddied@gmail.com>
2020-02-19 11:05:26 -0500

    13.2 - Env Variables in React

--- react-frontend/src/places/pages/UserPlaces.js
+++ react-frontend/src/places/pages/UserPlaces.js
@@ -15,7 +15,7 @@ const UserPlaces = () => {
     const fetchPlaces = async () => {
       try {
         const repsonseData = await sendRequest(
-          `http://localhost:5000/api/places/user/${userId}`
+          `${process.env.REACT_APP_BACKEND_URL}/places/user/${userId}`
         );
         setLoadedPlaces(repsonseData.userPlaces);
       } catch (error) {}
